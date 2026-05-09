d661e7130546365ec6bd160e252b88764ceedc78
Muthupandi M <mmuthupandi05@outlook.com>
2026-05-09 12:03:24 +0530

    api keys are stored

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDbUg406e2aIFv38dNd0AkzoJ_wf7SlBaM",
+  authDomain: "mm-tech-academy.firebaseapp.com",
+  projectId: "mm-tech-academy",
+  storageBucket: "mm-tech-academy.firebasestorage.app",
+  messagingSenderId: "260169759514",
+  appId: "1:260169759514:web:909903696cf61d03174aa3",
+  measurementId: "G-M6BDF7PRE8"
 };
 
 // Initialize Firebase
